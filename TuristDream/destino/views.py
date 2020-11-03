4b7eb6b1fe76ebc9522d2f44cd920d4b44234f7d
unknown <ignac.gonzalezs@alumnos.duoc.cl>
2020-11-03 03:05:11 -0300

    TuristDream:Creando pagina de Inicio

--- TuristDream/destino/views.py
+++ TuristDream/destino/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from . models import Destino, Ciudad
+from django.views import generic
 
 # Create your views here.
+
+def index(request):
+    num_destino=Destino.objects.all()
+    
+    return render(
+        request,
+        'index.html',
+        context={'num_destino':num_destino},
+    )
+# Create your views here. 
+
+def about(request):
+    
+    
+    return render(
+        request,
+        'about.html',
+        
+    )
+
+def contact(request):
+    
+    return render(
+        request,
+        'contact.html',
+    )
+
+
+
