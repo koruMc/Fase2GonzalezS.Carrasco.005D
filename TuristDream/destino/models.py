2e4beed79d36798151f3a68995239e76f8516503
unknown <ignac.gonzalezs@alumnos.duoc.cl>
2020-11-02 23:07:12 -0300

    Creando Modelos

---
+++ TuristDream/destino/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.urls import reverse
+import uuid
+
+# Create your models here.
+
+class Ciudad(models.Model):
+	"""Model representing an author."""
+	name = models.CharField(max_length=100)
+	summary = models.TextField(max_length=1000)
+
+	class Meta:
+		ordering = ['name']
+
+	def get_absolute_url(self):
+		return reverse('ciudad-detail', args=[str(self.id)])
+
+	def __str__(self):
+		"""String for representing the Model object."""
+		return self.name
+
+
+class Destino(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=200)
+    ciudad = models.ForeignKey('Ciudad', on_delete=models.SET_NULL, null=True, blank=False)
+    summary = models.TextField(max_length=1000)
+    url = models.URLField(max_length=100, default='')
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+
+    
+
+    class Meta:
+        ordering=['name']
+        
+    def __str__(self):
+        #return f'{self.id} ({self.title})'
+        return self.name
+    
+    def get_absolute_url(self):
+        """Returns the url to access a detail record for this book."""
+        return reverse('Destino-detail', args=[str(self.id)])
